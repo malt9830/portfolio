5c83e3d6482a735739846c242c034706f079856d
malt9830 <70881301+malt9830@users.noreply.github.com>
2021-01-07 04:38:31 +0100

    header

--- script.js
+++ script.js
@@ -8,6 +8,31 @@ window.onscroll = function () {
         document.querySelector("#header").classList.remove("active");
     }
 }
+
 function sidenVises() {
     console.log("sidenVises");
+    document.querySelector("#burger").addEventListener("click", showBurger);
+    document.querySelector("#burgerOff").addEventListener("click", hideBurger);
+}
+
+function showBurger() {
+    console.log("showBurger");
+    document.querySelector("#burger").classList.add("hidden");
+    document.querySelector("#burgerOff").classList.remove("hidden");
+
+    document.querySelector("#burgerItems").classList.remove("hidden");
+
+    document.querySelector("#header").classList.add("active");
+}
+
+function hideBurger() {
+    console.log("hideBurger");
+    document.querySelector("#burger").classList.remove("hidden");
+    document.querySelector("#burgerOff").classList.add("hidden");
+
+    document.querySelector("#burgerItems").classList.add("hidden");
+
+    if (window.scrollY < 75) {
+        document.querySelector("#header").classList.remove("active");
+    }
 }
