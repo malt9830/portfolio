592fd1e57de04d3f0984f40943e79135a621ac78
malt9830 <70881301+malt9830@users.noreply.github.com>
2021-01-07 02:21:45 +0100

    landing

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 window.addEventListener("load", sidenVises);
 
+window.onscroll = function () {
+    console.log("scroll");
+    if (window.scrollY > 75) {
+        document.querySelector("#header").classList.add("active");
+    } else {
+        document.querySelector("#header").classList.remove("active");
+    }
+}
 function sidenVises() {
     console.log("sidenVises");
 }
